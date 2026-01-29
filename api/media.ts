c94b2257d4945842d0ec1aedabdd4327c219cafc
kevalll97-blip <kevalll97@gmail.com>
2026-01-28 20:21:44 -0500

    Update media.ts

--- api/media.ts
+++ api/media.ts
@@ -79,6 +79,7 @@ export default {
         const jsonResponse = await handleUpload({
           body,
           request,
+          token: getBlobToken(),
           onBeforeGenerateToken: async (pathname) => {
             return {
               allowedContentTypes: [
